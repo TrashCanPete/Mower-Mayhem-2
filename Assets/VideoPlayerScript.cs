7d4400291d14ea751f10bf7104cb77801a10444f
Peter McDermott <mcdermpete@gmail.com>
2020-06-24 11:18:47 +1200

    Menu video player scene. Works as webGL build

---
+++ Assets/VideoPlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayerScript : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Menu Gameplay loop_V2.mp4");
+        videoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
